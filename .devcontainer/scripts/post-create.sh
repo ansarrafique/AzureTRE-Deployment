58fa6468ce726cc1ecfd4401e32a9a880cf47e16
Ansar Rafique <ansark6@gmail.com>
2026-02-17 15:02:14 +0000

    Initial commit

--- .devcontainer/scripts/post-create.sh
+++ .devcontainer/scripts/post-create.sh
@@ -13,14 +13,3 @@ cp ~/AzureTRE/config.sample.yaml .
 
 # docker socket fixup
 sudo bash AzureTRE/devops/scripts/set_docker_sock_permission.sh
-
-# --- v0.27.0 PORTER CACHE FIX ---
-# PR #4827 uses --cache-to/--cache-from flags. These require a 'docker-container' driver.
-# Since we are inside a dev container, we must create a local builder instance.
-echo "Initializing Buildx tre-builder for Porter compatibility..."
-if ! docker buildx ls | grep -q "tre-builder"; then
-    docker buildx create --name tre-builder --driver docker-container --use
-    docker buildx inspect --bootstrap
-else
-    docker buildx use tre-builder
-fi
